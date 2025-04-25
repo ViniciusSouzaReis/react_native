0d118ec5ff06c51cc86c61bfa7ee47abf60696c5
Vinicius Reis <vinniiesouza@gmail.com>
2025-04-24 22:40:02 -0300

    a

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,21 +1,45 @@
-import { type ReactElement, useState } from 'react';
+import { type ReactElement, useState, Component } from 'react';
 
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, Image } from 'react-native';
 
-export default function App(): ReactElement {
-  const [count, setCount] = useState(0);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
 
+    this.adicionar = this.adicionar.bind(this);
+  }
+
+  adicionar() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render(): ReactElement {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', color: '#fff' }}>
+        <Jobs height={300} width={300}/>
+        <Text>Hello World !!!!</Text>
+        <Text style={{ color: '#ff0000', fontSize: 25}}>{this.state.count}</Text>
+        <Button
+          title="Press me"
+          style={{ marginTop: 20 }}
+          color="#841584"
+          onPress={this.adicionar}
+        />
+      </View>
+    );
+  }
+ 
+}
+
+function Jobs({ height, width}): ReactElement {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', color: '#fff' }}>
-      <Text>{'Hello World !!!!'}</Text>
-      <Text>{count}</Text>
-      <Button
-        title="Press me"
-        style={{ marginTop: 20 }}
-        color="#841584"
-        onPress={() => setCount(count + 1)}
-      />
-    </View>
+    <Image 
+      source={{ uri: 'https://sujeitoprogramador.com/steve.png' }}
+      style={{ width, height, borderRadius: 100 }}
+    />
   );
 }
 
